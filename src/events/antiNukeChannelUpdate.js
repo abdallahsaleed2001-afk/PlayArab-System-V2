10d18b3448859071af6c4fd17a7c5f12ec36dc9e
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-09-07 08:45:52 +0300

    security: remove unconfigured channel update handler

--- src/events/antiNukeChannelUpdate.js
+++
@@ -1,9 +0,0 @@
-import { handleAntiNuke } from '../services/security/antiNuke.js';
-
-export default {
-  name: 'channelUpdate',
-  async execute(channel) {
-    if (!channel?.guild) return;
-    await handleAntiNuke(channel.guild, 'channelUpdate', channel.id);
-  },
-};
